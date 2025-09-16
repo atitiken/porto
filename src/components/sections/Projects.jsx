4b42353cd307e76d3f7d8e40e569d449be4a794e
atitiken <rielfel1@gmail.com>
2025-09-16 15:27:19 +0700

    added links

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -35,7 +35,7 @@ export const Projects = () => {
                  Brain tumor computer classification using efficientnet-b1.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["OpenCV", "Python", "Mediapipe"].map((tech, key) => (
+                {["Python", "Pytorch", "Pandas"].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
@@ -86,12 +86,12 @@ Imbalanced Fracture Detection</h3>
               </div>
             </div>
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-            <h3 className="text-xl font-bold mb-2">[New Project Title]</h3>
+            <h3 className="text-xl font-bold mb-2">[Pose Detection Using AI Model]</h3>
             <p className="text-gray-400 mb-4">
-              [Your new project description goes here. For example: "Staff work efficiency monitoring using realtime pose detection with improved dashboard visualization."]
+              [Staff work efficiency monitoring using realtime pose detection.]
             </p>
             <div className="flex flex-wrap gap-2 mb-4">
-              {["React", "TailwindCSS", "Node.js"].map((tech, key) => (
+              {["OpenCV", "Python", "Mediapipe"].map((tech, key) => (
                 <span
                   key={key}
                   className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
@@ -102,7 +102,7 @@ Imbalanced Fracture Detection</h3>
             </div>
             <div className="flex justify-between items-center">
               <a
-                href="https://github.com/your-username/new-project"
+                href="https://github.com/erilneten/AOL_AIhehehe"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-400 hover:text-blue-300 transition-colors my-4"
